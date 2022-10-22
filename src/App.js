e3f0a6a38149001e6db18d64a68231e47df444b8
aakj2010 <aakj2010@gmail.com>
2022-10-22 19:45:28 +0530

    today

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -20,6 +22,7 @@ function App() {
         </div>
 
       </BrowserRouter>
+      <ToastContainer />
     </>
   );
 }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+
+function Spinner() {
+    return (
+        <div className='loadingSpinnerContainer'>
+            <div className="loadingSpinnerContainer"></div>
+
+        </div>
+    )
+}
+
+export default Spinner
